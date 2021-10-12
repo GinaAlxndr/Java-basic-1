99f199aa593507289d6f5518338f21d28bce7ee6
gina khalisa <khalisa.gina18@gmail.com>
2021-10-12 14:09:11 +0700

    first commit

---
+++ javachallange.java
@@ -0,0 +1,20 @@
+public class javachallange{
+    public static void main (String[] args){
+    // int number = 1234;
+    // System.out.println(firstName + number);
+    String firstName = "Gina";
+    String lastName = "Khalisa";
+    String age = "20";
+    String addresString = "jl. setiabudi";
+    String phoneNumber = "082246575752";
+    String hobi = "nonton";
+    String makananKesukaan = "bakso";
+    System.out.println("first name : " + firstName);
+    System.out.println("last name : " + lastName);
+    System.out.println("Age : " + age);
+    System.out.println("address : " + addresString);
+    System.out.println("phoneNumber : " + phoneNumber);
+    System.out.println("hobi : " + hobi);
+    System.out.println("makanan kesukaan : " + makananKesukaan);
+    }
+}
